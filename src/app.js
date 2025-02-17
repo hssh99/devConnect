e3e14cf30890385bac1073c4deccbb24022682d7
hssh99 <145898285+hssh99@users.noreply.github.com>
2025-02-17 23:09:41 +0530

    added request handlers & created custom scripts in package.json

--- src/app.js
+++ src/app.js
@@ -2,4 +2,14 @@ const express = require("express");
 
 const app = express();
 
-app.listen(3000);
+app.use("/hello", (req, res) => {
+    res.send("Hello from the server!");
+})
+
+app.use("/test", (req, res) => {
+    res.send("Hello from test!");
+})
+
+app.listen(3000, () => {
+    console.log("Server is successfully listening on port 3000...")
+});
